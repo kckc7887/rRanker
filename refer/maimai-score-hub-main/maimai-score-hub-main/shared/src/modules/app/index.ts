847a83e37aba34ccd16ef84f1e37d78b0cb95db4
尘言 <126900157+kckc7887@users.noreply.github.com>
2026-07-11 21:23:24 +0800

    Delete refer directory

--- refer/maimai-score-hub-main/maimai-score-hub-main/shared/src/modules/app/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./app.contract";
-export * from "./app.schema";
--- refer/maimai-score-hub-main/maimai-score-hub-main/shared/src/modules/job/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './job.schema';
-export * from './job-extra.schema';
-export * from './job-priority';
-export * from './job.contract';
